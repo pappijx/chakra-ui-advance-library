4ee535337031ae6ba2edd9b8314783266f72c65e
pappijx <ayush.papani123@gmail.com>
2023-02-21 23:04:49 +0530

    push all library changes

--- packages/jx-multiselect/src/index.tsx
+++ packages/jx-multiselect/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Jxcheckbox } from 'jx-checkbox'
 import { TItem, TItemsList } from './types'
 import styled from 'styled-components'
